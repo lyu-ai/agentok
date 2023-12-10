e7370571fa8b6b598de561272ff7a9fb09c08ff0
Hugh Lyu <hugh.lv@gmail.com>
2023-12-11 02:02:52 +0800

    Hide theme switcher until styles are checked

--- frontend/src/app/[locale]/components/Navbar.tsx
+++ frontend/src/app/[locale]/components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
         <NavMenu />
       </div>
       <div className="navbar-end flex items-center gap-2">
-        <ThemeSwitcher />
+        {/* <ThemeSwitcher /> */}
         {isSupabaseConnected && <AuthButton />}
       </div>
     </div>
