c70d4739b28a4568f678e9bd0dc044fe68db7b28
Hugh Lyu <hugh.lv@gmail.com>
2023-12-12 02:16:13 +0800

    test env var

--- frontend/src/utils/supabase/client.ts
+++ frontend/src/utils/supabase/client.ts
@@ -1,7 +1,13 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from '@supabase/ssr';
+
+console.log('NEXT_PUBLIC_SUPABASE_URL', process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log(
+  'NEXT_PUBLIC_SUPABASE_ANON_KEY',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
 export const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+  );
