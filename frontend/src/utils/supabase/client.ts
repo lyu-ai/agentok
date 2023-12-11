a0d743129610c97f89a04cf7849f3a408dff1e94
Hugh Lyu <hugh.lv@gmail.com>
2023-12-12 02:37:01 +0800

    testing env

--- frontend/src/utils/supabase/client.ts
+++ frontend/src/utils/supabase/client.ts
@@ -1,5 +1,12 @@
 import { createBrowserClient } from '@supabase/ssr';
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and Anon Key must be provided.');
+}
+
 console.log('NEXT_PUBLIC_SUPABASE_URL', process.env.NEXT_PUBLIC_SUPABASE_URL);
 console.log(
   'NEXT_PUBLIC_SUPABASE_ANON_KEY',
@@ -7,7 +14,4 @@ console.log(
 );
 
 export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  createBrowserClient(supabaseUrl, supabaseAnonKey);
