2b9871672c9c285e10578221887bb48a725852ec
Hugh Lyu <hugh.lv@gmail.com>
2023-12-28 21:35:56 +0800

    docs: renaming

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'FlowGen Docs',
+  title: 'FlowGen',
   tagline: 'AutoGen Visualized - Visual Tools for Multi-Agent Development.',
   favicon: 'img/favicon.ico',
 
