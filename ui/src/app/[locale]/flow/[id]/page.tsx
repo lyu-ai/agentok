b7b54145e0a1da9fd1dc2b242c07e1a081d293b4
Hugh Lyu <hugh.lv@gmail.com>
2024-01-04 14:06:18 +0800

    feat: open flows in tabs #48

--- ui/src/app/[locale]/flow/[id]/page.tsx
+++ ui/src/app/[locale]/flow/[id]/page.tsx
@@ -2,17 +2,22 @@
 
 import { ReactFlowProvider } from 'reactflow';
 import Autoflow from '../../components/Flow';
-import { useFlow } from '@/hooks';
-import { useEffect } from 'react';
+import { useFlow, useFlows } from '@/hooks';
+import { use, useEffect } from 'react';
 
 const Page = ({ params }: { params: { id: string } }) => {
   const { flow } = useFlow(params.id);
+  const { activeFlowId, openFlow } = useFlows();
 
   useEffect(() => {
     if (flow?.name && typeof window !== 'undefined') {
       document.title = `${flow.name} | FlowGen`;
     }
   }, [flow?.name]);
+
+  useEffect(() => {
+    openFlow(params.id);
+  }, [params.id]);
 
   return (
     <ReactFlowProvider key="reactflow-flow">
