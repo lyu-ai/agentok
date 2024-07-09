ff1dd271ae5bbd8a270b78380a1f3295c83b5c26
Hugh Lyu <hugh.lv@gmail.com>
2024-07-09 16:55:14 +0800

    feat: quick button for creating project

--- ui/src/app/[locale]/components/navbar/ProjectPicker.tsx
+++ ui/src/app/[locale]/components/navbar/ProjectPicker.tsx
@@ -10,11 +10,17 @@ import {
 import clsx from 'clsx';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { RiApps2Line, RiArrowDownSLine } from 'react-icons/ri';
+import {
+  RiAddCircleLine,
+  RiAddLine,
+  RiApps2Line,
+  RiArrowDownSLine,
+} from 'react-icons/ri';
+import { toast } from 'react-toastify';
 
 const ProjectPicker = ({ activeProjectId, className }: any) => {
   const router = useRouter();
-  const { projects } = useProjects();
+  const { projects, createProject, isCreating } = useProjects();
   const { project } = useProject(activeProjectId);
   const [activeProject, setActiveProject] = useState<
     Project | null | undefined
@@ -24,38 +30,61 @@ const ProjectPicker = ({ activeProjectId, className }: any) => {
     setActiveProject(project);
     setAvailableProjects(projects);
   }, [project, projects]);
+  const onCreateProject = async () => {
+    const newProject = await createProject();
+    if (!newProject) {
+      toast.error('Failed to create project');
+      return;
+    }
+    toast.success('Project created. Now jumping to project page.');
+    router.push(`/projects/${newProject.id}/flow`);
+  };
   return (
-    <Listbox
-      value={activeProject}
-      onChange={v => router.push(`/projects/${v?.id}/flow`)}
-    >
-      <ListboxButton
+    <div className="ml-2 flex items-center gap-0.5">
+      <Listbox
+        value={activeProject}
+        onChange={v => router.push(`/projects/${v?.id}/flow`)}
+      >
+        <ListboxButton
+          className={clsx(
+            'flex gap-1 items-center bg-base-content/5',
+            className
+          )}
+        >
+          <RiApps2Line className="h-4 w-4" />
+          <span className="text-ellipsis overflow-hidden whitespace-nowrap hidden lg:block">
+            {activeProject?.name}
+          </span>
+          <RiArrowDownSLine className="h-4 w-4" />
+        </ListboxButton>
+        <ListboxOptions
+          anchor="bottom start"
+          className="flex flex-col p-2 bg-base-200 dark:bg-gray-700 rounded mt-1 min-w-48 gap-1 shadow dark:border dark:border-gray-600"
+        >
+          {availableProjects.map(project => (
+            <ListboxOption
+              key={project.id}
+              value={project}
+              className="group flex gap-2 text-sm rounded px-4 py-2 data-[selected]:border data-[selected]:border-base-content/20 data-[focus]:bg-base-content/10 cursor-pointer"
+            >
+              {project.name}
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </Listbox>
+      <button
+        onClick={onCreateProject}
         className={clsx(
-          'ml-2 flex gap-1 items-center bg-base-content/5',
+          'w-8 h-7.5  flex items-center justify-center cursor-pointer',
           className
         )}
+        data-tooltip-id="default-tooltip"
+        data-tooltip-content="Create Project"
       >
-        <RiApps2Line className="h-4 w-4" />
-        <span className="text-ellipsis overflow-hidden whitespace-nowrap hidden lg:block">
-          {activeProject?.name}
-        </span>
-        <RiArrowDownSLine className="h-4 w-4" />
-      </ListboxButton>
-      <ListboxOptions
-        anchor="bottom start"
-        className="flex flex-col p-2 bg-base-200 dark:bg-gray-700 rounded mt-1 min-w-48 gap-1 shadow dark:border dark:border-gray-600"
-      >
-        {availableProjects.map(project => (
-          <ListboxOption
-            key={project.id}
-            value={project}
-            className="group flex gap-2 text-sm rounded px-4 py-2 data-[selected]:border data-[selected]:border-base-content/20 data-[focus]:bg-base-content/10 cursor-pointer"
-          >
-            {project.name}
-          </ListboxOption>
-        ))}
-      </ListboxOptions>
-    </Listbox>
+        {!isCreating && <RiAddLine className="w-5 h-5" />}
+        {isCreating && <div className="loading loading-primary w-5 h-5" />}
+      </button>
+    </div>
   );
 };
 
--- ui/src/app/[locale]/projects/page.tsx
+++ ui/src/app/[locale]/projects/page.tsx
@@ -4,8 +4,13 @@ import { useRouter } from 'next/navigation';
 import ProjectList from '../components/ProjectList';
 import { useProjects } from '@/hooks/useProjects';
 import { toast } from 'react-toastify';
+import { useTranslations } from 'next-intl';
+import { RiSwap3Line, RiShoppingBag4Line } from 'react-icons/ri';
+import Link from 'next/link';
+import TemplateList from '../components/TemplateList';
 
 const Page = () => {
+  const t = useTranslations('page.Projects');
   const router = useRouter();
   const { createProject } = useProjects();
   const onCreateProject = async () => {
@@ -19,12 +24,28 @@ const Page = () => {
   };
   return (
     <div className="flex flex-col w-full gap-2">
-      <div className="flex items-center justify-end">
-        <button onClick={onCreateProject} className="btn btn-primary">
-          Create Project
+      <title>Projects | Agentok Studio</title>
+      <div className="flex flex-col items-center justify-center gap-2 text-sm p-2">
+        <span className="text-5xl font-bold p-4">{t('tagline')}</span>
+        <span className="text-lg p-4">{t('projects-description')}</span>
+        <button onClick={onCreateProject} className="btn btn-lg btn-primary">
+          <RiSwap3Line className="w-7 h-7" />
+          {t('create-project')}
         </button>
       </div>
       <ProjectList />
+      <div className="divider text-2xl">Or</div>
+      <div className="flex flex-col items-center justify-center gap-2 text-sm py-8 mb-12">
+        <RiShoppingBag4Line className="w-12 h-12" />
+        <span className="text-2xl p-4">{t('start-from-template')}</span>
+        <TemplateList maxCount={3} />
+        <Link
+          href="/marketplace"
+          className="flex flex-col items-center link link-lg link-primary py-8 gap-4 text-2xl"
+        >
+          {t('discover-marketplace')}
+        </Link>
+      </div>
     </div>
   );
 };
