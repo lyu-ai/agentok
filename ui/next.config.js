e44e21856fa89048b30d5333ce922b8abe7b9da6
Hugh Lyu <hugh.lv@gmail.com>
2024-07-31 12:56:53 +0800

    chore: Test env var support in next.config

--- ui/next.config.js
+++ ui/next.config.js
@@ -20,6 +20,10 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
