adcdd3f46959b10c2a01551017e1551631026dee
Hugh Lyu <hugh.lv@gmail.com>
2024-08-01 17:06:07 +0800

    fix: Avatar in account page.

--- ui/src/app/[locale]/settings/account/page.tsx
+++ ui/src/app/[locale]/settings/account/page.tsx
@@ -1,32 +1,11 @@
 'use client';
-import { useEffect, useState } from 'react';
-import supabase from '@/utils/supabase/client'; // Adjust the import path as necessary
 import Loading from '@/components/Loading';
-import { User } from '@supabase/supabase-js';
+import { useUser } from '@/hooks';
 
 const AccountPage = () => {
-  const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [avatar, setAvatar] = useState('');
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      await supabase.auth.getUser().then(resp => {
-        const user = resp.data?.user;
-        setUser(user);
-      }).catch(err => {
-        setError(err.message);
-      }).finally(() => {
-        setLoading(false);
-      });
-    };
-
-    fetchUserData();
-  }, []);
+  const { user, loading } = useUser();
 
   if (loading || !user) return <Loading />;
-  if (error) return <div>Error: {error}</div>;
 
   return (
     <div className="w-full p-2">
@@ -34,19 +13,19 @@ const AccountPage = () => {
       <div className="flex flex-col gap-6 border rounded-lg p-6 w-full border-base-content/20 bg-gradient-to-r from-transparent to-base-content/20">
         <div className="avatar">
           <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src={avatar} alt="User avatar" />
+            <img src={user.user_metadata?.avatar_url ?? '/logo-spaced.png'} alt="User avatar" />
           </div>
         </div>
         <div className="flex-grow">
           <div className="mb-4">
-            <span className="font-bold">Name:</span> {user.email}
+            <span className="font-bold">Name:</span> {user.user_metadata?.full_name ?? user.email?.match(/^([^@]+)/)?.[1] ?? '(No Name)'}
           </div>
           <div className="mb-4">
             <span className="font-bold">Email:</span> {user.email}
           </div>
           <div>
-            <span className="font-bold">Created:</span>{' '}
-            {new Date(user.created_at).toLocaleDateString()}
+            <span className="font-bold">Since:</span>{' '}
+            {new Date(user.created_at).toLocaleString()}
           </div>
         </div>
       </div>
