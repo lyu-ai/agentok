4d6787a7acf0a1db679d6298b5266a51d89e8b3b
Hugh Lyu <hugh.lv@gmail.com>
2024-08-14 11:50:41 +0800

    ci: Set workers to count of CPU.

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -7,4 +7,4 @@ source .venv/bin/activate
 UVICORN_WORKERS=${UVICORN_WORKERS:-$(python -c 'import multiprocessing; print(multiprocessing.cpu_count())')}
 
 # Start the Uvicorn server
-exec uvicorn agentok_api.main:app --host ${UVICORN_HOST:-0.0.0.0} --port ${UVICORN_PORT:-5004} --workers 1 --log-level ${UVICORN_LOG_LEVEL:-debug}
+exec uvicorn agentok_api.main:app --host ${UVICORN_HOST:-0.0.0.0} --port ${UVICORN_PORT:-5004} --workers ${UVICORN_WORKERS} --log-level ${UVICORN_LOG_LEVEL:-debug}
