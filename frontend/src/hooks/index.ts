0ab24a4002e49e818f9cbbb4d62427f7ad7c8c77
Hugh Lyu <hugh.lv@gmail.com>
2024-11-20 00:44:59 +0800

    chore: removed rag related files

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './useProjects';
 export * from './useTemplates';
-export * from './useDatasets';
 export * from './useTools';
 export * from './useChats';
 export * from './useMediaQuery';
