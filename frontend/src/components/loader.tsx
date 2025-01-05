a0458df0b02bc430639947e6e1793f260f2ea719
Hugh Lyu <hugh.lv@gmail.com>
2025-01-05 11:44:16 +0800

    chore: say goodbye to spinner

--- frontend/src/components/loader.tsx
+++ frontend/src/components/loader.tsx
@@ -3,7 +3,7 @@ import { Icons } from './icons';
 export const Loading = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <Icons.spinner className="animate-spin" />
+      <Icons.logoSimple className="h-10 w-10 animate-pulse text-muted-foreground/20" />
     </div>
   );
 };
